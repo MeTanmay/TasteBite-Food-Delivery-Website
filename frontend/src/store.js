28053397d481b2c42a8a045f86a94e49a2c8b115
Tanmay Borse <tanmayborse28@gmail.com>
2024-08-30 00:33:58 +0530

    complete update-profile page functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,7 +9,7 @@ import {
 import thunk from 'redux-thunk';        // helps to run the data fetching (delayed work)
 import { restaurantReducer } from './reducer/restaurantReducer';
 import { menuReducer } from './reducer/menuReducer';
-import { authReducer } from './reducer/userReducer';
+import { authReducer, userReducer } from './reducer/userReducer';
 
 // create store -> just like array or object
 // combineReducers -> to group the reducer
@@ -20,6 +20,7 @@ const reducer = combineReducers({
     restaurants: restaurantReducer,
     menus: menuReducer,
     auth: authReducer,
+    user: userReducer
 });
 
 // helps us to connect the reactDevTools 
